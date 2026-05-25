458c3ef6d088b66461c9635031b607f08b7a6675
Salar <lrdsallymail@gmail.com>
2026-05-26 00:29:31 +0330

    build db pool

---
+++ db/pool.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import aiomysql
+import os
+
+# ---- ENJOY CODING TODAY ----
+
+load_dotenv()
+
+DB_CONFIG = {
+    "host": "localhost", 
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "db": os.getenv("DB_NAME")
+}
+
+pool = None
+async def initialize_pool():
+    global pool
+    pool = await aiomysql.create_pool(
+        **DB_CONFIG,
+        minimize = 1,
+        maximize = 5
+    )
+
+async def fetchone(query, params):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(query, params)
+            return await cur.fetchone()
+
+async def execute(query, params):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(query, params)
+            await conn.commit()
+
+async def execute_return_id(query, params):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(query, params)
+            await conn.commit()
+            return cur.lastrowid()
